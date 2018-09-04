020d47631be1fe4f4e1d0e572d89b9878dd57f2f
Geary <fgwitera@github.com>
2018-09-04 13:19:21 +0800

    Change functions to plugins and Add battery plugins

--- bash_profile
+++ bash_profile
@@ -1,5 +1,5 @@
 # Load the shell dotfiles
-for file in ~/.{prompt,aliases,functions}; do
+for file in ~/.{prompt,aliases,plugins}; do
     [ -r "$file" ] && [ -f "$file" ] && source "$file";
 done;
 unset file;
--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc vim bashrc bash_profile aliases functions prompt"    # list of files/folders to symlink in homedir
+files="vimrc vim bashrc bash_profile aliases plugins prompt"    # list of files/folders to symlink in homedir
 
 ##########
 
